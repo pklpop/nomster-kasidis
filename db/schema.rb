bd702f27945b067ec1a8dbecb8b6e9cd5ca22c42
Kasidis <pklpop@hotmail.com>
2015-07-25 10:03:37 +0000

    uploader done

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20150722142935) do
 
   create_table "photos", force: true do |t|
     t.text     "caption"
+    t.integer  "user_id"
+    t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
@@ -61,7 +63,6 @@ ActiveRecord::Schema.define(version: 20150722142935) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
